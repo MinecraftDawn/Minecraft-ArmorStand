100a79800d45e314882381200ef057cad38776c6
MinecraftDawn <eric4902077@gmail.com>
2021-08-08 17:37:59 +0800

    reverse left and right of skin

--- 圖片切割/圖片切割.py
+++ 圖片切割/圖片切割.py
@@ -1,10 +1,10 @@
 import cv2 as cv
 img = cv.imread('diamond_layer_1.png', cv.IMREAD_UNCHANGED)
 
-# 左右上下前後
+# 右左上下前後
 
-helmets = [[8,16,0,8],
-           [8,16,16,24],
+helmets = [[8,16,16,24],
+           [8,16,0,8],
            [0,8,8,16],
            [0,8,0,8],
            [8,16,8,16],
@@ -15,8 +15,8 @@ for i in range(len(helmets)):
      helmet = img[s[0]:s[1],s[2]:s[3]]
      cv.imwrite(f'helmet{i}.png', helmet)
 
-chestplates =  [[20,32,16,20],
-                [20,32,28,32],
+chestplates =  [[20,32,28,32],
+                [20,32,16,20],
                 [0,1,0,1],
                 [0,1,0,1],
                 [20,32,20,28],
